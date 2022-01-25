ce3db732b30b4dac08b7f45944a15d01763bda94
betancourtg <98134622+betancourtg@users.noreply.github.com>
2022-01-25 13:14:21 -0600

    ejercicio 1

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const ejercicio1 = [
   3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
   19, 88, 456, 13, 23, 24,
 ];
+const esPrimo = num => {
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) return false;
 
+  } return true
+}
+ejercicio1.forEach(num => {
+  console.log("¿%d es primo? %s", num, esPrimo(num));
+})
 const ejercicio2 = [
   {
     nombre: "Gabriel",
