aca283c5d7efd12ca9b5ae24b1ba5f7bb5972b54
betancourtg <98134622+betancourtg@users.noreply.github.com>
2022-01-29 18:55:23 -0600

    EjerciciosJS terminados

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const ejercicio1 = [
   3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
   19, 88, 456, 13, 23, 24,
 ];
-
+/*
 function primo (a) {
   if (a <= 1){
     return false
-  } else{
-    for (let i = 2; i<a;i++){
-      if(a%i==0){
+  } else {
+    for (let i = 2; i < a ; i ++ ){
+      if ( a % i == 0 ){
         return false
       }
     }
@@ -16,14 +16,16 @@ function primo (a) {
   }
 }
 function numeroPrimo(){
-  ejercicio1.forEach(function(a){
-    if(primo(a)){
-      console.log("el número"+ a + "es primo")
+  ejercicio1.forEach(function numeroPrimo(a){
+    if (primo(a)){
+      console.log("El número " + a + " es primo")
     }
   })
 }
-numeroPrimo
 
+numeroPrimo();
+
+*/
 
 const ejercicio2 = [
   {
@@ -57,3 +59,26 @@ const ejercicio2 = [
     esFamiliar: true,
   },
 ];
+
+function admitido(){
+  ejercicio2.forEach(function (i){
+    if(i.edad >= 18 && i.esFamiliar == true){
+      console.log(i.nombre + " es admitido")
+    }
+  })
+}
+
+admitido()
+
+// Fibonacci
+let x=0;
+let y=1;
+let z = 0;
+console.log(y);
+
+for (let fibonacci = 0; fibonacci <= 50; fibonacci ++) {
+  z= x+y;
+  console.log(z);
+  x=y;
+  y=z;
+};
